dcc5fc01eaf0b49daaf9d3366ddb8fa08e271a29
nevitatriya <triyanevita7@gmail.com>
2025-04-16 13:26:07 +0700

    Edit by Nevita

--- Transaksi.java
+++ Transaksi.java
@@ -1,27 +1,30 @@
 public class Transaksi {
-    String kodeTransaksi, tanggalTransaksi, type;
+    String kodeTransaksi, tanggalTransaksi, tipe;
     double saldo, inOutSaldo, finalSaldo;
-    Bank bankAcc;
+    Bank bankAcc = new Bank();
 
     public Transaksi(String kodeTransaksi, double saldo, double inOutSaldo, double finalSaldo,
-        String tanggalTransaksi, String type, Bank bankAcc) {
+        String tanggalTransaksi, String tipe, Bank bankAcc) {
         this.kodeTransaksi = kodeTransaksi;
         this.saldo = saldo;
         this.inOutSaldo = inOutSaldo;
         this.finalSaldo = finalSaldo;
         this.tanggalTransaksi = tanggalTransaksi;
-        this.type = type;
+        this.tipe = tipe;
         this.bankAcc = bankAcc;
+        
+    }
+    
+    public void tampilDataTransaksi() {
+        System.out.printf("%-10s %-15s %-10s %-5s%n",kodeTransaksi, bankAcc.noRekening, 
+        bankAcc.nama, saldo, inOutSaldo, finalSaldo, tanggalTransaksi,tipe);
+        // System.out.println("Kode Transaksi: " + kodeTransaksi);
+        // System.out.println("No Rekening: " + bankAcc.noRekening);
+        // System.out.println("Saldo: " + saldo);
+        // System.out.println("Debit/Kredit: " + inOutSaldo);
+        // System.out.println("Final Saldo: " + finalSaldo);
+        // System.out.println("Tanggal: " + tanggalTransaksi);
+        // System.out.println("Type: " + tipe);
     }
 
-    void tampilDataTransaksi() {
-        System.out.println("Kode Transaksi: " + kodeTransaksi);
-        System.out.println("Tanggal: " + tanggalTransaksi);
-        System.out.println("Type: " + type);
-        System.out.println("Saldo: " + saldo);
-        System.out.println("Debit/Kredit: " + inOutSaldo);
-        System.out.println("Final Saldo: " + finalSaldo);
-        bankAcc.tampilDataNorek();
-        System.out.println("-----------------------------");
-    }
 }
