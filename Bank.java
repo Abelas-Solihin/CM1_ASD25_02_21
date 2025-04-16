f52648bb28fc62b41f6aebdf9892308e4d29b9e8
Abelas-Solihin <abelassolihin@gmail.com>
2025-04-16 09:37:35 +0700

    Edit by Solihin

---
+++ Bank.java
@@ -0,0 +1,12 @@
+public class Bank {
+    String noRekening, nama, namaIbu, noHp, email;
+
+    public Bank(String noRekening, String nama, String namaIbu, String noHp, String email) {
+        this.noRekening = noRekening;
+        this.nama = nama;
+        this.namaIbu = namaIbu;
+        this.noHp = noHp;
+        this.email = email;
+    }
+
+}
